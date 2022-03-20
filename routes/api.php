7c5ef1ec0b19a7de5cfffae74152eac920e16f88
3m1n3nc3 <mygames.ng@gmail.com>
2022-03-20 10:56:02 +0100

    Updated profile update route

--- routes/api.php
+++ routes/api.php
@@ -161,7 +161,7 @@ Route::middleware(['auth:sanctum'])->group(function() {
     ->prefix('account')->name('account.')
     ->group(function() {
         Route::get('/', 'index')->name('index');
-        Route::post('/', 'update')->name('store');
+        Route::post('/update', 'store')->name('update');
         Route::get('/transactions', 'transactions')->name('transactions');
         Route::get('/savings ', 'savings')->name('index');
 
