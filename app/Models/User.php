16115472650aeb7ae57b001dd63dda8507ae34f2
3m1n3nc3 <mygames.ng@gmail.com>
2022-03-23 13:49:18 +0100

    Profile image bug fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,7 +64,7 @@ class User extends Authenticatable
     protected function imageUrl(): Attribute
     {
         $image = $this->image
-            ? img($this->image, 'banner', 'large-square')
+            ? img($this->image, 'banner', 'medium-square')
             : 'https://ui-avatars.com/api/?name='.urlencode($this->firstname.' '.$this->lastname).'&background=0D8ABC&color=fff&size=150&font-size=0.95&bold=true';
 
         return Attribute::make(
