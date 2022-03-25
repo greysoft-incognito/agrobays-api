88d1d75915a6fa08ab5972a076f8306b7a4031e0
3m1n3nc3 <mygames.ng@gmail.com>
2022-03-25 06:22:56 +0100

    Bug Fixes

--- routes/api.php
+++ routes/api.php
@@ -189,10 +189,10 @@ Route::middleware(['auth:sanctum'])->group(function() {
             // Savings Controller Routes
             Route::controller(SubscriptionController::class)
             ->group(function() {
+                Route::get('/subscriptions/data/{plan_id?}', 'dataTable')->name('data');
                 Route::get('/subscriptions/{limit?}/{status?}', 'index');
                 Route::post('/subscriptions/{limit?}/{status?}');
                 Route::post('/update-bag/subscription/{subscription_id}/bag/{id}', 'updateBag');
-                Route::get('/subscriptions/data/{plan_id?}', 'dataTable')->name('data');
                 Route::get('/subscription/{subscription_id?}', 'subscription');
             });
         });
