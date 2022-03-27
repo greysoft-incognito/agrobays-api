23bf3580688c0b69e95e8980c3b17508a3c4e0b6
3m1n3nc3 <mygames.ng@gmail.com>
2022-03-27 12:07:31 +0100

    Barcharts tooltip formater update

--- app/Actions/Greysoft/Charts.php
+++ app/Actions/Greysoft/Charts.php
@@ -64,7 +64,7 @@ class Charts
         return [
             "tooltip" => [
                 "trigger" => "axis",
-                "formatter" => "{b} <br/> {a0}: {c0}<br />{a1}: {c1}",
+                "formatter" => "{b} <br/> {a0}: {c0}<br />{a1}: {c1} <br/> {a2}: {c2}<br />{a3}: {c4}",
                 "axisPointer" => [
                     "type" => "shadow", // The default is a straight line, optional:'line' |'shadow'
                 ],
