9c0e58a504cf94faf64ee071d9b9981d0c0be8ff
3m1n3nc3 <mygames.ng@gmail.com>
2022-03-27 12:02:11 +0100

    Barcharts fully implemented and working format data

--- app/Actions/Greysoft/Charts.php
+++ app/Actions/Greysoft/Charts.php
@@ -64,7 +64,7 @@ class Charts
         return [
             "tooltip" => [
                 "trigger" => "axis",
-                "formatter" => "{a1} <br/>{b}: {$currency_symbol}{c} ({d}%)",
+                "formatter" => "{b0}: {c0}<br />{b1}: {c1}",
                 "axisPointer" => [
                     "type" => "shadow", // The default is a straight line, optional:'line' |'shadow'
                 ],
