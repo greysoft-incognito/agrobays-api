ee6a6024b9d7040b9e41c3544e59c9fb0400a634
3m1n3nc3 <mygames.ng@gmail.com>
2022-03-27 10:35:40 +0100

    Create bar chart method and set test data

--- app/Actions/Greysoft/Charts.php
+++ app/Actions/Greysoft/Charts.php
@@ -56,7 +56,7 @@ class Charts
     }
 
     public function bar(array $data)
-    {   
+    {
         return [
             "tooltip" => [
                 "trigger" => "axis",
@@ -89,22 +89,30 @@ class Charts
                 [
                     "name" => "Transactions",
                     "type" => "bar",
-                    "data" => [40, 45, 27, 50, 32, 50, 70, 30, 30, 40, 67, 29],
+                    "data" => collect(range(1,12))->map(function($get) {
+                            return rand(100, 1500);
+                        })->toArray(),
                     "color" => "#546bfa",
                 ], [
                     "name" => "Subscriptions",
                     "type" => "bar",
-                    "data" => [124, 100, 20, 120, 117, 70, 110, 90, 50, 90, 20, 50],
+                    "data" => collect(range(1,12))->map(function($get) {
+                        return rand(100, 1500);
+                    })->toArray(),
                     "color" => "#3a9688",
                 ], [
                     "name" => "Food Orders",
                     "type" => "bar",
-                    "data" => [17, 2, 0, 29, 20, 10, 23, 0, 8, 20, 11, 30],
+                    "data" => collect(range(1,12))->map(function($get) {
+                        return rand(100, 1500);
+                    })->toArray(),
                     "color" => "#02a9f4",
                 ], [
                     "name" => "Savings",
                     "type" => "bar",
-                    "data" => [20, 100, 80, 14, 90, 86, 100, 70, 120, 50, 30, 60],
+                    "data" => collect(range(1,12))->map(function($get) {
+                        return rand(100, 1500) . "m: $get";
+                    })->toArray(),
                     "color" => "#f88c2b",
                 ]
             ],
