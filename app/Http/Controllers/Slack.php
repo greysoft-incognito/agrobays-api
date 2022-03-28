419d25ec1f4786e6797544429fc6273d9b66d097
3m1n3nc3 <mygames.ng@gmail.com>
2022-03-28 10:27:48 +0100

    bug fixes

--- app/Http/Controllers/Slack.php
+++ app/Http/Controllers/Slack.php
@@ -71,7 +71,7 @@ class Slack extends Controller
     protected function msg($msg) 
     {
         $request = request();
-
+dd($request->response_url);
         if ($request->response_url) {
             $client = new \GuzzleHttp\Client(['base_uri' => $request->response_url]);
             $client->request('POST', '/', [
