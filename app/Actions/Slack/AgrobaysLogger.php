ad16d7e562f8cd4bafdabb88f39f7223dd02fd83
3m1n3nc3 <mygames.ng@gmail.com>
2022-03-28 12:12:59 +0100

    Improved Slack communications and fixed bugs

--- app/Actions/Slack/AgrobaysLogger.php
+++ app/Actions/Slack/AgrobaysLogger.php
@@ -9,7 +9,7 @@ use Spatie\SlashCommand\Handlers\BaseHandler;
 class AgrobaysLogger extends BaseHandler
 {
     protected $description = 'Toggle debug logs on or off for requests originating from Slack. This action accepts one of two parameters: {on|off}.';
-    
+
     public function canHandle(Request $request): bool
     {
         return str_is($request->command, 'agrobays-logger') && in_array($request->text, ['on', 'off']);
@@ -25,5 +25,6 @@ class AgrobaysLogger extends BaseHandler
             ->withAttachment(Attachment::create()
                 ->setColor('good')
                 ->setText("Slack request logs are now turned {$request->text}!")
+            );
     }
 }
