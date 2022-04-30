1d841f365f00e87d95600ccf436e6368fe3aa07b
3m1n3nc3 <mygames.ng@gmail.com>
2022-04-30 03:07:11 +0100

    Add fruit_bay_categories.sql to seeders

--- database/seeders/FruitBayCategorySeeder.php
+++ database/seeders/FruitBayCategorySeeder.php
@@ -15,36 +15,41 @@ class FruitBayCategorySeeder extends Seeder
      */
     public function run()
     {
-        FruitBayCategory::insert([
-            [
-                'slug' => 'citrus',
-                'title' => 'Citrus',
-                'description' => 'Citrus fruits includes important crops such as oranges, lemons, grapefruits, pomelos, and limes.',
-                'image' => 'public/uploads/images/1013334087_2019582335.png',
-                'created_at' => \Carbon\Carbon::now(),
-            ], [
-                'slug' => 'berries',
-                'title' => 'Berries',
-                'description' => 'Small, juicy fruits with thin skins
-                Highly perishable
-                Some berries include: blackberries, cranberries, blueberries, red and black raspberries, strawberries, and grapes.',
-                'image' => 'public/uploads/images/1455791222_799698722.png',
-                'created_at' => \Carbon\Carbon::now(),
-            ], [
-                'slug' => 'tropical',
-                'title' => 'Tropical',
-                'description' => 'Grown in warm climatesand are considered tobe somewhat exotic
-                Available throughout the world
-                Some tropical fruits include: avocados, coconut, bananas, figs, dates, guavas, mangoes, papayas, pineapples, pomegranates, and kiwi',
-                'image' => 'public/uploads/images/1886411736_955619728.png',
-                'created_at' => \Carbon\Carbon::now(),
-            ], [
-                'slug' => 'vegitables',
-                'title' => 'Vegitables',
-                'description' => 'Vegitables are beautiful food staples',
-                'image' => 'public/uploads/images/896311396_1718470427.png',
-                'created_at' => \Carbon\Carbon::now(),
-            ],
-        ]);
+        if (file_exists($path = base_path('database/seeders/fruit_bay_categories.sql'))) {
+            $sql = file_get_contents($path);
+            DB::unprepared($sql);
+        } else {
+            FruitBayCategory::insert([
+                [
+                    'slug' => 'citrus',
+                    'title' => 'Citrus',
+                    'description' => 'Citrus fruits includes important crops such as oranges, lemons, grapefruits, pomelos, and limes.',
+                    'image' => 'public/uploads/images/1013334087_2019582335.png',
+                    'created_at' => \Carbon\Carbon::now(),
+                ], [
+                    'slug' => 'berries',
+                    'title' => 'Berries',
+                    'description' => 'Small, juicy fruits with thin skins
+                    Highly perishable
+                    Some berries include: blackberries, cranberries, blueberries, red and black raspberries, strawberries, and grapes.',
+                    'image' => 'public/uploads/images/1455791222_799698722.png',
+                    'created_at' => \Carbon\Carbon::now(),
+                ], [
+                    'slug' => 'tropical',
+                    'title' => 'Tropical',
+                    'description' => 'Grown in warm climatesand are considered tobe somewhat exotic
+                    Available throughout the world
+                    Some tropical fruits include: avocados, coconut, bananas, figs, dates, guavas, mangoes, papayas, pineapples, pomegranates, and kiwi',
+                    'image' => 'public/uploads/images/1886411736_955619728.png',
+                    'created_at' => \Carbon\Carbon::now(),
+                ], [
+                    'slug' => 'vegitables',
+                    'title' => 'Vegitables',
+                    'description' => 'Vegitables are beautiful food staples',
+                    'image' => 'public/uploads/images/896311396_1718470427.png',
+                    'created_at' => \Carbon\Carbon::now(),
+                ],
+            ]);
+        }
     }
 }
---
+++ database/seeders/fruit_bay_categories.sql
@@ -0,0 +1,16 @@
+-- Adminer 4.8.1 MySQL 10.5.15-MariaDB-cll-lve dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+INSERT INTO `fruit_bay_categories` (`id`, `title`, `slug`, `description`, `image`, `created_at`, `updated_at`) VALUES
+(1,	'Citrus',	'citrus',	'Citrus fruits includes important crops such as oranges, lemons, grapefruits, pomelos, and limes.',	'public/uploads/images/1350553858_1426771530.png',	'2022-04-27 05:28:36',	'2022-04-27 11:14:45'),
+(2,	'Berries',	'berries',	'Small, juicy fruits with thin skins\r\n Highly perishable\r\n Some berries include: blackberries, cranberries, blueberries, red and black raspberries, strawberries, and grapes.',	'public/uploads/images/969801131_388295637.png',	'2022-04-27 05:28:36',	'2022-04-27 11:14:55'),
+(3,	'Tropical',	'tropical',	'Grown in warm climatesand are considered tobe somewhat exotic\r\n Available throughout the world\r\n Some tropical fruits include: avocados, coconut, bananas, figs, dates, guavas, mangoes, papayas, pineapples, pomegranates, and kiwi',	'public/uploads/images/1723440104_1044367169.png',	'2022-04-27 05:28:36',	'2022-04-27 11:15:16'),
+(4,	'Vegitables',	'vegitables',	'Vegitables are beautiful food staples',	'public/uploads/images/1812552803_647725341.png',	'2022-04-27 05:28:36',	'2022-04-27 11:15:27');
+
+-- 2022-04-30 02:04:10
